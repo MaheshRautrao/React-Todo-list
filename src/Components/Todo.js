53b2cc1f37711d5091d9c74961ad73e3251ee164
RutCD <rutchulin0@gmail.com>
2023-11-26 23:17:56 -0500

    delete product

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,9 +1,11 @@
 import React from 'react'
 //export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
   import { useLayoutEffect, useState } from "react"
-export  const Todo = () => {
+export  const Todo = ({newNotification}) => {
       
       const [products, setProducts] = useState([]);
+      const [dialog, setDialog] = useState(false)
+      const [productDelete, setProductDelete] = useState({})
       async function getProducts(){
           const response = await fetch("/api/products-get");
           const productsApi = await response.json();
@@ -19,10 +21,43 @@ export  const Todo = () => {
               {
                   products.map((product) => <li key={product.id}>
                       {product.name}: <br />
-                      {product.description} <b>{product.quantity}</b> 
-                  </li>)
+                      {product.description} <b>{product.quantity}</b>
+                      <button onClick={()=>{
+                        setDialog(true)
+                        setProductDelete(product)
+                    }}>Eliminar</button>
+                </li>)
               }
           </ul>
+
+            <dialog open={dialog}>
+            <h3>¿Deseas eliminar el producto?</h3>
+            <div>
+                <span>{productDelete.name}</span> 
+                <small>{productDelete.quantity}</small>
+            </div>
+            <button onClick={()=>{setDialog(false); setProductDelete({})}}>cancelar</button>
+            <button onClick={async()=>{
+                const response = await fetch("/api/products-delete", {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        id: productDelete.id
+                    })
+                }).catch((error) => console.error("Error:", error))
+                const status = await response.json()
+                if(status.deleted){
+                    setProducts([])
+                    getProducts()
+                    newNotification(status.message)
+                    setDialog(false)
+                }
+            }}>Eliminar</button>
+            </dialog>
+
+
       </section>
   }
 
