29712e8550f20f63d95f92347c5393306934e88d
RutCD <rutchulin0@gmail.com>
2023-11-26 19:31:45 -0500

    add products

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -3,20 +3,30 @@ import React, {useState} from 'react'
 export const TodoForm = ({addTodo}) => {
     const [value, setValue] = useState('');
 
-    const handleSubmit = (e) => {
-      // prevent default action
-        e.preventDefault();
-        if (value) {
-          // add todo
-          addTodo(value);
-          // clear form after submission
-          setValue('');
-        }
-      };
   return (
-    <form onSubmit={handleSubmit} className="TodoForm">
-    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What is the task today?' />
-    <button type="submit" className='todo-btn'>Add Task</button>
+    <form onSubmit={async(e)=>{
+      e.preventDefault()
+      const response = await fetch("/api/products-post", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+              name,
+              description,
+              quantity
+          })
+      }).catch((error) => console.error("Error:", error))
+      const status = await response.json()
+      if(status.created){
+          newNotification(status.message)
+          navigate("/")
+      }
+
+      console.log("response", response);
+  }}>
+    <input type="text" setValue={setName} value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What is the task today?' />
+    <button type="submit" className='todo-btn'>Add Product</button>
   </form>
   )
 }
