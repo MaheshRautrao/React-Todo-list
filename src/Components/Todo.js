664b86b1fa7cb04cc93ebaad74af8ff46ef8d8ed
RutCD <rutchulin0@gmail.com>
2023-11-26 22:46:20 -0500

    component todo fix

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,12 +1,9 @@
 import React from 'react'
 //export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
   import { useLayoutEffect, useState } from "react"
-  import { useNavigate } from "react-router-dom";
-export  const Todo = ({newNotification, task, deleteTodo, editTodo, toggleComplete}) => {
-      const navigate = useNavigate()
+export  const Todo = () => {
+      
       const [products, setProducts] = useState([]);
-      const [dialog, setDialog] = useState(false)
-      const [productDelete, setProductDelete] = useState({})
       async function getProducts(){
           const response = await fetch("/api/products-get");
           const productsApi = await response.json();
@@ -23,39 +20,9 @@ export  const Todo = ({newNotification, task, deleteTodo, editTodo, toggleComple
                   products.map((product) => <li key={product.id}>
                       {product.name}: <br />
                       {product.description} <b>{product.quantity}</b> 
-                      <button onClick={()=>{navigate("/edit", {state: product})}}>Editar</button>
-                      <button onClick={()=>{
-                          setDialog(true)
-                          setProductDelete(product)
-                      }}>Eliminar</button>
                   </li>)
               }
           </ul>
-          <dialog open={dialog}>
-              <h3>¿Deseas eliminar el producto?</h3>
-              <div>
-                  <span>{productDelete.name}</span> 
-                  <small>{productDelete.quantity}</small>
-              </div>
-              <button onClick={()=>{setDialog(false); setProductDelete({})}}>cancelar</button>
-              <button onClick={async()=>{
-                  const response = await fetch("/api/products-delete", {
-                      method: "DELETE",
-                      headers: {
-                          "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify({
-                          id: productDelete.id
-                      })
-                  }).catch((error) => console.error("Error:", error))
-                  const status = await response.json()
-                  if(status.deleted){
-                      setProducts([])
-                      getProducts()
-                      newNotification(status.message)
-                      setDialog(false)
-                  }
-              }}>Eliminar</button>
-          </dialog>
       </section>
   }
+
--- src/Components/TodoWrapper.js
+++ src/Components/TodoWrapper.js
@@ -1,22 +1,10 @@
 import React, { useState } from "react";
-// import { Todo } from "./Todo";
+import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
 // import { EditTodoForm } from "./EditTodoForm";
-import { useLayoutEffect } from "react"
 
 export const TodoWrapper = () => {
-  const [products, setProducts] = useState([]);
-      // const [dialog, setDialog] = useState(false)
-      // const [productDelete, setProductDelete] = useState({})
-      async function getProducts(){
-          const response = await fetch("/api/products-get");
-          const productsApi = await response.json();
-          setProducts(productsApi)
-      }
-      useLayoutEffect(()=>{
-          getProducts()
-      },[])
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
@@ -26,53 +14,12 @@ export const TodoWrapper = () => {
     ]);
   }
 
-  // const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
-
-  // const toggleComplete = (id) => {
-  //   setTodos(
-  //     todos.map((todo) =>
-  //       todo.id === id ? { ...todo, completed: !todo.completed } : todo
-  //     )
-  //   );
-  // }
-
-  // const editTodo = (id) => {
-  //   setTodos(
-  //     todos.map((todo) =>
-  //       todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
-  //     )
-  //   );
-  // }
-
-  // const editTask = (task, id) => {
-  //   setTodos(
-  //     todos.map((todo) =>
-  //       todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
-  //     )
-  //   );
-  // };
-
   return (
     <div className="TodoWrapper">
-      <h1>Get Things Done !</h1>
+      <h1>Lista de productos!</h1>
       <TodoForm addTodo={addTodo} />
       {/* display todos */}
-      hola
-      <h3>Products</h3>
-          {products.length === 0 && <span>Agrega Productos</span>}
-          <ul>
-              {
-                  products.map((product) => <li key={product.id}>
-                      {product.name}: <br />
-                      {product.description} <b>{product.quantity}</b> 
-                      {/* <button onClick={()=>{navigate("/edit", {state: product})}}>Editar</button> */}
-                      {/* <button onClick={()=>{
-                          setDialog(true)
-                          setProductDelete(product)
-                      }}>Eliminar</button> */}
-                  </li>)
-              }
-          </ul>
+      <Todo />
     </div>
   );
 };
